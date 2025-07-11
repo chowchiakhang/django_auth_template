fd47041473283be61116f26ee252fc1d7c7ada47
Charles Chow <you@example.com>
2025-07-11 22:18:31 +0800

    feat: create user model

--- auth/users/models.py
+++ auth/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    """
+    Custom user model that extends the default Django user model.
+    You can add additional fields here if needed.
+    """
+    # Example of adding a custom field
+    # bio = models.TextField(blank=True, null=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
