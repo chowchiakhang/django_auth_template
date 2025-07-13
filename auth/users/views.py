fc04f96b88309fbafb7f1aa92e9cd23da5193c3c
Charles Chow <you@example.com>
2025-07-13 17:31:56 +0800

    feat: register user

--- auth/users/views.py
+++ auth/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serialiizers import UserSerializer
 
 # Create your views here.
+class RegisterView(APIView):
+    """
+    View to handle user registration.
+    """
+    def post(self, request):
+        # Logic for user registration will go here
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
