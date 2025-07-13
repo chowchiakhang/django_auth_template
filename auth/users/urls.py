f7aa5a096eeb8cbe2cb7a9f6040a6c5d4c2db391
Charles Chow <you@example.com>
2025-07-13 22:02:01 +0800

    feat: authenticate user

--- auth/users/urls.py
+++ auth/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import LoginView, RegisterView
+from .views import LoginView, RegisterView, UserView
 urlpatterns = [
     path('register', RegisterView.as_view()),
     path('login', LoginView.as_view()),
+    path('user', UserView.as_view()),
 ]
